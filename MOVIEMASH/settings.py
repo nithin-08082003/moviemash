48069650a4f9e724e02c728870afc16881805a12
nithin <nithinkgowdanithinkgowda@gmail.com>
2022-10-11 10:26:38 +0530

    database

--- MOVIEMASH/settings.py
+++ MOVIEMASH/settings.py
@@ -89,6 +89,9 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
